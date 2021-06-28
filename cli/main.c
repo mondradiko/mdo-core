ddeef2680ffff6448355ed74b72872fa50bf18f3
marceline-cramer <cramermarceline@gmail.com>
2021-06-28 09:27:15 -0600

    Refactor cli main.c and init GPU

--- cli/main.c
+++ cli/main.c
@@ -1,28 +1,60 @@
 #include <stdio.h>
 
 #include "displays/sdl/sdl_display.h"
+#include "gpu/gpu_device.h"
+
+typedef struct cli_state_s
+{
+  sdl_display_t *dp;
+  gpu_device_t *gpu;
+} cli_state_t;
+
+int
+init_cli_state (cli_state_t *cli)
+{
+  cli->dp = NULL;
+  cli->gpu = NULL;
+
+  if (gpu_device_new (&cli->gpu))
+    {
+      fprintf (stderr, "failed to create GPU device\n");
+      return 1;
+    }
+
+  if (sdl_display_new (&cli->dp))
+    {
+      fprintf (stderr, "failed to create SDL display\n");
+      return 1;
+    }
+
+  return 0;
+}
+
+void
+cleanup_cli_state (cli_state_t *cli)
+{
+  if (cli->dp)
+    sdl_display_delete (cli->dp);
+
+  if (cli->gpu)
+    gpu_device_delete (cli->gpu);
+}
 
 int
 main ()
 {
-  sdl_display_t *dp = NULL;
-  if (sdl_display_new (&dp))
-    {
-      /* TODO(marceline-cramer): custom logging */
-      fprintf (stderr, "failed to create SDL display\n");
-    }
-  else
-    {
-      struct display_poll_t poll;
-      poll.should_exit = 0;
-      while (!poll.should_exit)
-        {
-          sdl_display_poll (dp, &poll);
-        }
-    }
+  cli_state_t cli;
 
-  if (dp)
-    sdl_display_delete (dp);
+  int result = init_cli_state (&cli);
+  if (result)
+    return result;
+
+  struct display_poll_t poll;
+  poll.should_exit = 0;
+  while (!poll.should_exit)
+    {
+      sdl_display_poll (cli.dp, &poll);
+    }
 
   return 0;
 }
