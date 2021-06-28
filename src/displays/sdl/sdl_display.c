dbb3129679c8c4374a2f0445b6d2573242ecdf5b
marceline-cramer <cramermarceline@gmail.com>
2021-06-28 08:38:08 -0600

    Create SDL window

--- src/displays/sdl/sdl_display.c
+++ src/displays/sdl/sdl_display.c
@@ -4,6 +4,8 @@
 #include "displays/sdl/sdl_display.h"
 
 /* TODO(marceline-cramer): replace with mdo_allocator */
+#include <SDL2/SDL_error.h>
+#include <stdio.h>  /* for fprintf */
 #include <stdlib.h> /* for mem alloc*/
 
 #include <SDL2/SDL.h>
@@ -19,11 +21,25 @@ sdl_display_new (sdl_display_t **new_dp)
   sdl_display_t *dp = malloc (sizeof (sdl_display_t));
   *new_dp = dp;
 
+  dp->window = SDL_CreateWindow ("Mondradiko Core", SDL_WINDOWPOS_UNDEFINED,
+                                 SDL_WINDOWPOS_UNDEFINED, 800, 600,
+                                 SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+
+  if (!dp->window)
+    {
+      /* TODO(marceline-cramer): custom logging */
+      fprintf (stderr, "failed to create SDL window: %s\n", SDL_GetError ());
+      return -1;
+    }
+
   return 0;
 }
 
 void
 sdl_display_delete (sdl_display_t *dp)
 {
+  if (dp->window)
+    SDL_DestroyWindow (dp->window);
+
   free (dp);
 }
