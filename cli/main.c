2e57253a9df0529f507a72c7f4ab69d862df0263
marceline-cramer <cramermarceline@gmail.com>
2021-06-28 08:28:57 -0600

    Init/destroy SDL display in CLI

--- cli/main.c
+++ cli/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello world!\n");
+#include "displays/sdl/sdl_display.h"
+
+int
+main ()
+{
+  sdl_display_t *dp = NULL;
+  if (sdl_display_new (&dp))
+    {
+      /* TODO(marceline-cramer): custom logging */
+      fprintf (stderr, "failed to create SDL display\n");
+    }
+
+  if (dp)
+    sdl_display_delete (dp);
+
   return 0;
 }
