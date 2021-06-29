235160353150ecd7d03723b5a7e6d0cf59edc09a
marceline-cramer <cramermarceline@gmail.com>
2021-06-29 03:21:41 -0600

    Add base renderer_t

---
+++ include/renderer/renderer.h
@@ -0,0 +1,23 @@
+/** @file renderer.h
+ */
+
+#pragma once
+
+#include "gpu/gpu_device.h"
+#include "renderer/camera.h"
+
+/** @typedef renderer_t
+ */
+typedef struct renderer_s renderer_t;
+
+/** @function renderer_new
+ */
+int renderer_new (renderer_t **, gpu_device_t *);
+
+/** @function renderer_delete
+ */
+void renderer_delete (renderer_t *);
+
+/** @function renderer_render_frame
+ */
+void renderer_render_frame (renderer_t *);
---
+++ src/renderer/renderer.c
@@ -0,0 +1,20 @@
+/** @file renderer.c
+ */
+
+#include "renderer/renderer.h"
+
+int
+renderer_new (renderer_t **new_ren, gpu_device_t *gpu)
+{
+  return 0;
+}
+
+void
+renderer_delete (renderer_t *ren)
+{
+}
+
+void
+renderer_render_frame (renderer_t *ren)
+{
+}
