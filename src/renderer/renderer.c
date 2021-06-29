d10366449fe0f19a55a9e12ff5a77dbfe6a5f3ec
marceline-cramer <cramermarceline@gmail.com>
2021-06-29 04:14:10 -0600

    Add vkQueuePresentKHR

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -9,6 +9,7 @@
 #include <stdlib.h> /* for mem alloc */
 #include <vulkan/vulkan_core.h>
 
+#include "gpu/gpu_device.h"
 #include "renderer/render_phases.h"
 
 #define MAX_CAMERA_NUM 1024
@@ -23,6 +24,7 @@ struct renderer_s
 {
   gpu_device_t *gpu;
   VkDevice vkd;
+  VkQueue present_queue;
   struct frame_data frames[MAX_FRAMES_IN_FLIGHT];
   int frame_num;
 };
@@ -63,6 +65,10 @@ renderer_new (renderer_t **new_ren, gpu_device_t *gpu)
   ren->gpu = gpu;
   ren->vkd = gpu_device_get (gpu);
 
+  int gfx_family = gpu_device_gfx_family (gpu);
+  int queue_index = 0;
+  vkGetDeviceQueue (ren->vkd, gfx_family, queue_index, &ren->present_queue);
+
   ren->frame_num = 2;
   for (int i = 0; i < ren->frame_num; i++)
     frame_data_init (ren, &ren->frames[i]);
@@ -97,11 +103,33 @@ renderer_render_frame (renderer_t *ren, camera_t **cameras, int camera_num)
     }
 
   int swapchain_num = 0;
-  VkSwapchainKHR swapchains[MAX_CAMERA_NUM];
+  VkSwapchainKHR swapchains[MAX_VIEWPORT_NUM];
+  uint32_t image_indices[MAX_VIEWPORT_NUM];
   for (int i = 0; i < viewport_num; i++)
     {
       VkSwapchainKHR swapchain = viewport_get_swapchain (viewports[i]);
       if (swapchain != VK_NULL_HANDLE)
-        swapchains[swapchain_num++] = swapchain;
+        {
+          image_indices[swapchain_num]
+              = viewport_get_image_index (viewports[i]);
+          swapchains[swapchain_num] = swapchain;
+          swapchain_num++;
+        }
+    }
+
+  if (swapchain_num > 0)
+    {
+      VkPresentInfoKHR present_info = {
+        .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+        .waitSemaphoreCount = 0,
+        .swapchainCount = swapchain_num,
+        .pSwapchains = swapchains,
+        .pImageIndices = image_indices,
+      };
+
+      if (vkQueuePresentKHR (ren->present_queue, &present_info) != VK_SUCCESS)
+        {
+          fprintf (stderr, "failed to present to swapchains\n");
+        }
     }
 }
