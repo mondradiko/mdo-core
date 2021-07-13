d29c038965f18d6340edddc146bdbea0ca3f146e
marceline-cramer <cramermarceline@gmail.com>
2021-07-13 15:26:39 -0600

    Refactor cli/main.c
    Add command-line arguments
    Create and run `network_client`

--- cli/main.c
+++ cli/main.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "displays/display.h"
 #include "displays/sdl/sdl_display.h"
 #include "gpu/gpu_device.h"
 #include "gpu/vk_config.h"
 #include "log.h"
+#include "network/network_client.h"
 #include "renderer/renderer.h"
 #include "world/world.h"
 
 typedef struct cli_state_s
 {
+  /* params */
+  int is_headless;
+  int is_client;
+
+  /* objects */
   sdl_display_t *dp;
   gpu_device_t *gpu;
   renderer_t *ren;
   world_t *w;
+
+  union
+  {
+    network_client_t *client;
+  } network;
 } cli_state_t;
 
+void
+print_help (const char *argv0)
+{
+  fprintf (stderr, "Usage\n  %s [--headless] [--server]", argv0);
+}
+
 int
+parse_cli_args (cli_state_t *cli, int argc, const char *argv[])
+{
+  cli->is_headless = 0;
+  cli->is_client = 1;
+
+  for (int i = 1; i < argc; i++)
+    {
+      const char *arg = argv[i];
+
+      if (strcmp (arg, "--headless") == 0)
+        {
+          cli->is_headless = 1;
+        }
+      else if (strcmp (arg, "--server") == 0)
+        {
+          cli->is_client = 0;
+        }
+      else
+        {
+          print_help (argv[0]);
+          return 1;
+        }
+    }
+
+  return 0;
+}
+
+void
 init_cli_state (cli_state_t *cli)
 {
   cli->dp = NULL;
@@ -24,37 +70,69 @@ init_cli_state (cli_state_t *cli)
   cli->ren = NULL;
   cli->w = NULL;
 
-  if (sdl_display_new (&cli->dp))
+  if (cli->is_client)
+    cli->network.client = NULL;
+}
+
+int
+create_cli_objects (cli_state_t *cli)
+{
+
+  if (!cli->is_headless)
     {
-      LOG_ERR ("failed to create SDL display");
-      return 1;
+      if (sdl_display_new (&cli->dp))
+        {
+          LOG_ERR ("failed to create SDL display");
+          return 1;
+        }
+
+      struct vk_config_t vk_config;
+      sdl_display_vk_config (cli->dp, &vk_config);
+
+      if (gpu_device_new (&cli->gpu, &vk_config))
+        {
+          LOG_ERR ("failed to create GPU device");
+          return 1;
+        }
+
+      if (sdl_display_begin_session (cli->dp, cli->gpu))
+        {
+          LOG_ERR ("failed to begin SDL session");
+          return 1;
+        }
+
+      VkRenderPass rp
+          = camera_get_render_pass (sdl_display_get_camera (cli->dp));
+      if (renderer_new (&cli->ren, cli->gpu, rp))
+        {
+          LOG_ERR ("failed to create renderer");
+          return 1;
+        }
+
+      if (world_new (&cli->w, renderer_get_debug_draw_list (cli->ren)))
+        {
+          LOG_ERR ("failed to create world");
+          return 1;
+        }
     }
 
-  struct vk_config_t vk_config;
-  sdl_display_vk_config (cli->dp, &vk_config);
-
-  if (gpu_device_new (&cli->gpu, &vk_config))
+  if (cli->is_client)
     {
-      LOG_ERR ("failed to create GPU device");
-      return 1;
+      if (network_client_new (&cli->network.client))
+        {
+          LOG_ERR ("failed to create network client");
+          return 1;
+        }
+
+      if (network_client_connect (cli->network.client, "127.0.0.1", 10555))
+        {
+          LOG_ERR ("failed to connect to server");
+          return 1;
+        }
     }
-
-  if (sdl_display_begin_session (cli->dp, cli->gpu))
+  else
     {
-      LOG_ERR ("failed to begin SDL session");
-      return 1;
-    }
-
-  VkRenderPass rp = camera_get_render_pass (sdl_display_get_camera (cli->dp));
-  if (renderer_new (&cli->ren, cli->gpu, rp))
-    {
-      LOG_ERR ("failed to create renderer");
-      return 1;
-    }
-
-  if (world_new (&cli->w, renderer_get_debug_draw_list (cli->ren)))
-    {
-      LOG_ERR ("failed to create world");
+      LOG_ERR ("network server is not implemented yet");
       return 1;
     }
 
@@ -64,6 +142,12 @@ init_cli_state (cli_state_t *cli)
 void
 cleanup_cli_state (cli_state_t *cli)
 {
+  if (cli->is_client)
+    {
+      if (cli->network.client)
+        network_client_delete (cli->network.client);
+    }
+
   if (cli->w)
     world_delete (cli->w);
 
@@ -101,30 +185,38 @@ temporary_debug_draw (debug_draw_list_t *ddl)
 }
 
 int
-main ()
+main (int argc, const char *argv[])
 {
   cli_state_t cli;
 
-  int result = init_cli_state (&cli);
+  int result = parse_cli_args (&cli, argc, argv);
   if (result)
     return result;
 
-  struct display_poll_t poll;
-  poll.should_exit = 0;
-  while (!poll.should_exit)
+  init_cli_state (&cli);
+  result = create_cli_objects (&cli);
+  if (result)
+    return result;
+
+  if (!cli.is_headless)
     {
-      sdl_display_poll (cli.dp, &poll);
-
-      if (poll.should_run)
+      struct display_poll_t poll;
+      poll.should_exit = 0;
+      while (!poll.should_exit)
         {
-          world_step (cli.w, poll.dt);
-        }
+          sdl_display_poll (cli.dp, &poll);
 
-      if (poll.should_render)
-        {
-          camera_t *camera = sdl_display_camera (cli.dp);
-          temporary_debug_draw (renderer_get_debug_draw_list (cli.ren));
-          renderer_render_frame (cli.ren, &camera, 1);
+          if (poll.should_run)
+            {
+              world_step (cli.w, poll.dt);
+            }
+
+          if (poll.should_render)
+            {
+              camera_t *camera = sdl_display_camera (cli.dp);
+              temporary_debug_draw (renderer_get_debug_draw_list (cli.ren));
+              renderer_render_frame (cli.ren, &camera, 1);
+            }
         }
     }
 
