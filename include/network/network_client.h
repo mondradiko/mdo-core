efef31cd23f1845dd0d9d727670c2d085f230f52
marceline-cramer <cramermarceline@gmail.com>
2021-07-13 15:25:40 -0600

    Add basic network_client

---
+++ include/network/network_client.h
@@ -0,0 +1,20 @@
+/** @file network_client.h
+ */
+
+#pragma once
+
+/** @typedef network_client_t
+ */
+typedef struct network_client_s network_client_t;
+
+/** @function network_client_new
+ */
+int network_client_new (network_client_t **);
+
+/** @function network_client_delete
+ */
+void network_client_delete (network_client_t *);
+
+/** @function network_client_connect
+ */
+int network_client_connect (network_client_t *, const char *, int);
---
+++ src/network/network_client.c
@@ -0,0 +1,67 @@
+/** @file network_client.c
+ */
+
+#include "network/network_client.h"
+
+#include "log.h"
+
+/* TODO(marceline-cramer): custom mem alloc */
+#include <stdlib.h> /* for mem alloc */
+
+#include <uv.h>
+
+struct network_client_s
+{
+  uv_loop_t loop;
+
+  /* connection info */
+  uv_connect_t connect;
+  uv_tcp_t socket;
+};
+
+int
+network_client_new (network_client_t **new_client)
+{
+  network_client_t *client = malloc (sizeof (network_client_t));
+  *new_client = client;
+
+  /* TODO(marceline-cramer): error handling? */
+  uv_loop_init (&client->loop);
+  uv_tcp_init (&client->loop, &client->socket);
+
+  return 0;
+}
+
+void
+network_client_delete (network_client_t *client)
+{
+  uv_loop_close (&client->loop);
+
+  free (client);
+}
+
+static void
+on_connect (uv_connect_t *req, int status)
+{
+  LOG_MSG ("connect status: %d", status);
+}
+
+int
+network_client_connect (network_client_t *client, const char *address,
+                        int port)
+{
+  struct sockaddr_in dest;
+  uv_ip4_addr (address, port, &dest);
+
+  client->connect = (uv_connect_t){
+    .data = client,
+  };
+
+  /* TODO(marceline-cramer): error handling? */
+  uv_tcp_connect (&client->connect, &client->socket,
+                  (const struct sockaddr *)&dest, on_connect);
+
+  uv_run (&client->loop, UV_RUN_ONCE);
+
+  return 0;
+}
